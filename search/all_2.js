cb821d7c7687289cf6ecc789bb8837784802e57f
omar <romar.zaratan@gamil.com>
2024-03-21 14:56:08 +0800

    uodated website definitions

--- search/all_2.js
+++ search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['centerloadingwithtext_0',['centerLoadingWithText',['../basic_array_manipulation_8cpp.html#a64483af96ab551ae4f66d339789b3f74',1,'basicArrayManipulation.cpp']]],
-  ['centertext_1',['centerText',['../consoleprint_8h.html#a143e401a54621c5b49effffca7b40613',1,'consoleprint.h']]],
+  ['centertext_1',['centerText',['../consoleprint_8h.html#a946deb6f4685786b58ce7a06e22fe8e7',1,'consoleprint.h']]],
   ['consoleprint_2eh_2',['consoleprint.h',['../consoleprint_8h.html',1,'']]],
   ['createfile_3',['createFile',['../handle_file_8h.html#af7c567e2732f49ca6baba07cc30fc421',1,'handleFile.h']]],
   ['csvdata_4',['csvData',['../snippets_8h.html#a9a15a2c689af675a9cd4ad79f50e8cd5',1,'snippets.h']]]
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['centerloadingwithtext_0',['centerLoadingWithText',['../basic_array_manipulation_8cpp.html#a64483af96ab551ae4f66d339789b3f74',1,'basicArrayManipulation.cpp']]],
-  ['centertext_1',['centerText',['../consoleprint_8h.html#a143e401a54621c5b49effffca7b40613',1,'consoleprint.h']]],
+  ['centertext_1',['centerText',['../consoleprint_8h.html#a946deb6f4685786b58ce7a06e22fe8e7',1,'consoleprint.h']]],
   ['createfile_2',['createFile',['../handle_file_8h.html#af7c567e2732f49ca6baba07cc30fc421',1,'handleFile.h']]]
 ];
